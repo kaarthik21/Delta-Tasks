b55c3c0e937cc2e4bdeeeb6ae4b22d3cace180f7
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-04 15:16:21 +0530

    Update schedule.sh

--- sysAd/task1/schedule.sh
+++ sysAd/task1/schedule.sh
@@ -3,20 +3,21 @@
 s = $(date -I) 
 
 if [[ $(grep "$s" /home/future.txt) ]] ; then 
-echo “DATE    TIME”> /home/Jay_Jay/schedule.txt
-grep -hnr “$s” /home/future.txt > /home/Jay_Jay/schedule.txt
-for i in {01..30};do
-echo "date   time" > /home/sysAd_$i/schedule.txt
-echo "date    time" > /home/webDev_$i/schedule.txt
-echo "date    time" > /home/appDev_$i/schedule.txt
-grep -hnr "$s" /home/future.txt > /home/sysAd_$i/schedule.txt 
-grep -hnr "$s" /home/future.txt > /home/webDev_$i/schedule.txt 
-grep -hnr "$s" /home/future.txt > /home/appDev_$i/schedule.txt
-done
+  echo “DATE    TIME”> /home/Jay_Jay/schedule.txt
+  grep -hnr “$s” /home/future.txt > /home/Jay_Jay/schedule.txt
+  for i in {01..30};do
+    echo "date    time" > /home/sysAd_$i/schedule.txt
+    echo "date    time" > /home/webDev_$i/schedule.txt
+    echo "date    time" > /home/appDev_$i/schedule.txt
+    grep -hnr "$s" /home/future.txt > /home/sysAd_$i/schedule.txt 
+    grep -hnr "$s" /home/future.txt > /home/webDev_$i/schedule.txt 
+    grep -hnr "$s" /home/future.txt > /home/appDev_$i/schedule.txt
+  done
+
 else
-echo “date    time”> /home/Jay_Jay/schedule.txt
-for i in {01..30};do
-echo "date    time" > /home/sysAd_$i/schedule.txt
-echo "date    time" > /home/webDev_$i/schedule.txt
-echo "date    time" > /home/appDev_$i/schedule.txt;done
+  echo “date    time”> /home/Jay_Jay/schedule.txt
+  for i in {01..30};do
+    echo "date    time" > /home/sysAd_$i/schedule.txt
+    echo "date    time" > /home/webDev_$i/schedule.txt
+    echo "date    time" > /home/appDev_$i/schedule.txt;done
 fi
