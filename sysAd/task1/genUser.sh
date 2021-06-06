5f4194b0d9e1e61e85334cd3e7cd989b0b11dec2
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-07 01:50:48 +0530

    Update genUser.sh

--- sysAd/task1/genUser.sh
+++ sysAd/task1/genUser.sh
@@ -16,24 +16,49 @@ for i in {01..30};do
   usermod -g genUser webDev_$i
 done
 
-groupadd second_years
-groupadd third_years
-groupadd fourth_years
+groupadd second_years_sysAd
+groupadd second_years_webDev
+groupadd second_years_appDev
+groupadd third_years_sysAd
+groupadd third_years_webDev
+groupadd third_years_appDev
+groupadd fourth_years_sysAd
+groupadd fourth_years_webDev
+groupadd fourth_years_appDev
 
 for j in {01..10}; do 
-  usermod -a -G second_years sysAd_$j
-  usermod -a -G second_years appDev_$j
-  usermod -a -G second_years webDev_$j
+  usermod -a -G second_years_sysAd sysAd_$j
+done
+
+for j in {01..10}; do 
+  usermod -a -G second_years_webDev webDev_$j
+done
+
+for j in {01..10}; do 
+  usermod -a -G second_years_appDev appDev_$j
+done
+
+
+for k in {11..20};do
+  usermod -a -G third_years_sysAd sysAd_$k
 done
 
 for k in {11..20};do
-  usermod -a -G third_years sysAd_$k
-  usermod -a -G third_years appDev_$k
-  usermod -a -G third_years webDev_$k
+  usermod -a -G third_years_webDev webDev_$k
+done
+
+for k in {11..20};do
+  usermod -a -G third_years_appDev appDev_$k
 done
 
 for l in {21..30};do
-  usermod -a -G fourth_years sysAd_$l
-  usermod -a -G fourth_years appDev_$l
-  usermod -a -G fourth_years webDev_$l
+  usermod -a -G fourth_years_sysAd sysAd_$l
+done
+
+for l in {21..30};do
+  usermod -a -G fourth_years_webDev webDev_$l
+done
+
+for l in {21..30};do
+  usermod -a -G fourth_years_appDev appDev_$l
 done
