73feeaa1330bd8e595bf3715b28edd56781ebda0
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-13 23:29:48 +0530

    Update schedule.sh

--- sysAd/task1/schedule.sh
+++ sysAd/task1/schedule.sh
@@ -4,14 +4,14 @@ s=$(date -I)
 
 if [[ $(grep "$s" /home/future.txt) ]] ; then 
   echo “DATE    TIME”> /home/Jay_Jay/schedule.txt
-  grep -hnr “$s” /home/future.txt > /home/Jay_Jay/schedule.txt
+  grep “$s” /home/future.txt > /home/Jay_Jay/schedule.txt
   for i in {01..30};do
     echo "date    time" > /home/sysAd_$i/schedule.txt
     echo "date    time" > /home/webDev_$i/schedule.txt
     echo "date    time" > /home/appDev_$i/schedule.txt
-    grep -hnr "$s" /home/future.txt >> /home/sysAd_$i/schedule.txt 
-    grep -hnr "$s" /home/future.txt >> /home/webDev_$i/schedule.txt 
-    grep -hnr "$s" /home/future.txt >> /home/appDev_$i/schedule.txt
+    grep "$s" /home/future.txt >> /home/sysAd_$i/schedule.txt 
+    grep "$s" /home/future.txt >> /home/webDev_$i/schedule.txt 
+    grep "$s" /home/future.txt >> /home/appDev_$i/schedule.txt
   done
 
 else
