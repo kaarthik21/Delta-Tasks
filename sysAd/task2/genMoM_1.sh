59bca648854f73519fa15b982e50cdb96bdf007a
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-21 17:07:02 +0530

    Create genMoM_1.sh

---
+++ sysAd/task2/genMoM_1.sh
@@ -0,0 +1,15 @@
+mkdir /home/temp/second
+f=/home/temp/temp_*
+
+let x=1
+for i in $f; do	
+		egrep '_(01|02|03|04|05|06|07|08|09|10)' $i >> /home/temp/second/second_date_$x 
+		let x=x+1
+done
+
+g=/home/temp/second/*
+for i in $g; do
+	tail -1 $i >> /home/temp/lastsecondyear.txt
+done 
+
+awk -F "[, ]" '{print "Some random text..." >> "/home/"$1"/"$3"_mom"}' /home/temp/lastsecondyear.txt
