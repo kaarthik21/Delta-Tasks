cbd6c82729bad9b8038638c7963c0d0796a74d92
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-21 17:06:24 +0530

    Create attendance.sh

---
+++ sysAd/task2/attendance.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+cd /home
+
+echo “Do u want to input by dates?” 
+read char
+if [[ $char = y ]]; then
+	echo “Input two dates in yyyy-mm-dd” 
+	read -p "Enter date1: " date1
+	read -p "Enter date2: " date2
+	awk -v d1="$date1" -v d2="$date2" -F " - |, " '{if($2>=d1 && $2<=d2) print $0}' /home/attendance.log  > /home/attendance.txt
+
+	for i in {01..30} 
+	do
+		if grep -q sysAd_$i /home/attendance.txt; then :
+		else 
+		echo "sysAd_$i" >> /home/Jay_Jay/finalattendance.txt
+		fi
+	done
+	for j in {01..30} 
+	do
+		if grep -q webDev_$j /home/attendance.txt; then :
+		else 
+		echo "webDev_$j" >> /home/Jay_Jay/finalattendance.txt
+		fi
+	done
+	for k in {01..30}
+	do
+		if grep -q appDev_$k /home/attendance.txt; then :
+		else 
+		echo "appDev_$k" >> /home/Jay_Jay/finalattendance.txt
+		fi
+	done
+
+elif [[ $char = n ]]; then
+	s=$(date -I) 
+	awk -v x="$s" '$5 <= x' /home/attendance.log > /home/attendance.txt
+	awk -F"[, ]" '{print >> "temp_"$3".txt"}' /home/attendance.txt
+	mkdir temp
+	mv /home/temp_* temp
+	f="/home/temp/*"
+	for i in {01..30}; do	
+		for j in $f; do 
+			if grep -q sysAd_$i $j ; then :
+				else
+				echo -n "sysAd_$i		"	>> /home/Jay_Jay/finalattendance.txt
+				echo -n "$j" | awk -F'[_.]' '{print $2}' >> /home/Jay_Jay/finalattendance.txt
+				fi
+		done
+	done
+
+	for i in {01..30}; do	
+		for j in $f; do 
+			if grep -q webDev_$i $j ; then :
+				else
+				echo -n "webDev_$i		"	>> /home/Jay_Jay/finalattendance.txt
+				echo -n "$j" | awk -F'[_.]' '{print $2}' >> /home/Jay_Jay/finalattendance.txt				
+				fi
+		done
+	done
+
+	for i in {01..30}; do	
+		for j in $f; do 
+			if grep -q appDev_$i $j ; then :
+				else
+				echo -n "appDev_$i		"	>> /home/Jay_Jay/finalattendance.txt
+				echo -n "$j" | awk -F'[_.]' '{print $2}' >> /home/Jay_Jay/finalattendance.txt
+ 				fi
+		done
+	done
+
+			
+fi
+
+cd -
+
+echo "Absentees updates"
