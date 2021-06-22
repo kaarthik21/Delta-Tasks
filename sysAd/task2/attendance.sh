73938a33be409373823d84332a9bf7381302b160
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-23 00:28:47 +0530

    Update attendance.sh

--- sysAd/task2/attendance.sh
+++ sysAd/task2/attendance.sh
@@ -7,25 +7,25 @@ if [[ $char = y ]]; then
 	echo “Input two dates in yyyy-mm-dd” 
 	read -p "Enter date1: " date1
 	read -p "Enter date2: " date2
-	awk -v d1="$date1" -v d2="$date2" -F " - |, " '{if($2>=d1 && $2<=d2) print $0}' /home/attendance.log  > /home/attendance.txt
+	awk -v d1="$date1" -v d2="$date2" -F " - |, " '{if($2>=d1 && $2<=d2) print $0}' /server/attendance.log  > /server/attendance.txt
 
 	for i in {01..30} 
 	do
-		if grep -q sysAd_$i /home/attendance.txt; then :
+		if grep -q sysAd_$i /server/attendance.txt; then :
 		else 
 		echo "sysAd_$i" >> /home/Jay_Jay/finalattendance.txt
 		fi
 	done
 	for j in {01..30} 
 	do
-		if grep -q webDev_$j /home/attendance.txt; then :
+		if grep -q webDev_$j /server/attendance.txt; then :
 		else 
 		echo "webDev_$j" >> /home/Jay_Jay/finalattendance.txt
 		fi
 	done
 	for k in {01..30}
 	do
-		if grep -q appDev_$k /home/attendance.txt; then :
+		if grep -q appDev_$k /server/attendance.txt; then :
 		else 
 		echo "appDev_$k" >> /home/Jay_Jay/finalattendance.txt
 		fi
@@ -33,8 +33,8 @@ if [[ $char = y ]]; then
 
 elif [[ $char = n ]]; then
 	s=$(date -I) 
-	awk -v x="$s" '$5 <= x' /home/attendance.log > /home/attendance.txt
-	awk -F"[, ]" '{print >> "temp_"$3".txt"}' /home/attendance.txt
+	awk -v x="$s" '$5 <= x' /server/attendance.log > /server/attendance.txt
+	awk -F"[, ]" '{print >> "temp_"$3".txt"}' /server/attendance.txt
 	mkdir temp
 	mv /home/temp_* temp
 	f="/home/temp/*"
@@ -67,7 +67,6 @@ elif [[ $char = n ]]; then
  				fi
 		done
 	done
-
 			
 fi
 
