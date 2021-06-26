8d2c8580ecda88b1504f97e9c144a4ea8b75f45d
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-26 19:43:46 +0530

    Update and rename MoM.sql to MoM_database.sh

--- sysAd/task2/MoM_database.sh
+++ sysAd/task2/MoM_database.sh
@@ -1,5 +1,4 @@
-# Check location of secure_file_priv
-SELECT @@GLOBAL.secure_file_priv;
+# Check location of secure_file_priv by 'SELECT @@GLOBAL.secure_file_priv;' in mysql
 
 # Copy the MoM text file into secure_file_priv
 cp /home/Jay_Jay/MoM.txt /var/lib/mysql-files/MoM.txt
