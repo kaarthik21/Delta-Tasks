8d03f981315b5b623d026101cdf3796670e4e86b
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-06-26 19:39:38 +0530

    Create Minutesofmeeting.py

---
+++ sysAd/task2/Minutesofmeeting.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import pymysql
+
+# Connect to the database
+connection = pymysql.connect(host='localhost', user='root', passwd='', db='Minutesofmeeting' )
+cursor=connection.cursor()
+
+# Drop table if already exists
+cursor.execute("DROP TABLE IF EXISTS MoM")
+
+# Create table
+cursor.execute("CREATE TABLE MoM(Minutes_of_meeting VARCHAR(200), Username VARCHAR(20), Date VARCHAR(20) )")
+
+# Create a new record 
+cursor.execute("SET GLOBAL local_infile=1")
+insert_data = "LOAD DATA INFILE '/var/lib/mysql-files/MoM.txt' INTO TABLE MoM COLUMNS TERMINATED BY ' ' LINES TERMINATED BY '\n'"
+cursor.execute(insert_data)
+
+# Connection is not autocommit by default. So we must commit to save our changes.
+connection.commit()
+
+# Execute query
+sql = "SELECT * FROM MoM"
+cursor.execute(sql)
+
+# Fetch all the records
+result = cursor.fetchall()
+for i in result:
+    print(i)
+
+# Close datbase connection
+connection.close()
