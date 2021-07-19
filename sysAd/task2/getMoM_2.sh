91fc6c0b2e999fa418a2e16841c544413bb1fc42
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-07-19 13:57:57 +0530

    Update getMoM_2.sh

--- sysAd/task2/getMoM_2.sh
+++ sysAd/task2/getMoM_2.sh
@@ -4,11 +4,11 @@ read -p "Enter date 1: " date1
 read -p "Enter date 2: " date2
 awk -v d1="$date1" -v d2="$date2" -F " - |, " '{if($2>=d1 && $2<=d2) print $0}' /home/temp/lastsecondyear.txt  >> /home/temp/betweendates.txt
 
-x="1"
-while read line; do
-echo "$x"
-awk -F "[, ]" -v x=$x 'NR==x{print $1, $3}' /home/temp/betweendates.txt >> /home/Jay_Jay/MoM.txt
-awk -F "[, ]" -v x=$x 'NR==x{print "/home/"$1"/"$3"_mom"}' /home/temp/betweendates.txt | xargs cat >> /home/Jay_Jay/MoM.txt
+let x="1"
+while read line; do 
+a=$(awk -F "[, ]" -v x=$x 'NR==x{print $1, $3}' /home/temp/betweendates.txt) 
+b=$(awk -F "[, ]" -v x=$x 'NR==x{print "/home/"$1"/"$3"_mom"}' /home/temp/betweendates.txt | xargs cat) 
+echo $a $b
+
 ((x++))
 done < /home/temp/lastsecondyear.txt
-
