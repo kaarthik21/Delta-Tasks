6f373292aad28b5bb9fb99db117efcc3abb967f4
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-07-22 23:09:52 +0530

    Create moms.php

---
+++ sysAd/task2b/moms.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+        <title>moms.local</title>
+</head>
+
+<body>
+
+<?php
+$mysqli = new mysqli("localhost","root","KAARTHIK2002","Minutesofmeeting");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// Perform query
+if ($result = $mysqli -> query("SELECT * FROM MoM")) {
+  echo "Returned rows are: " . $result -> num_rows;
+  // Free result set
+  $result -> free_result();
+}
+
+$mysqli -> close();
+?>
+
+</body>
+</html>
