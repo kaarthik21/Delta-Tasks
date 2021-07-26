1afdb383970780f42c3cd35bb2659984d8308b10
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-07-26 13:08:59 +0530

    Create config.php

---
+++ sysAd/task2b/config.php
@@ -0,0 +1,26 @@
+<?php                                                                                                                                                                                       
+session_start(); $username = $password = $userError = $passError = '';                                                                                                                      
+if(isset($_POST['sub'])){                                                                                                                                                                   
+  $username = $_POST['username']; $password = $_POST['password'];                                                                                                                           
+  if($username === 'admin' && $password === 'superuserpass'){                                                                                                                               
+    $_SESSION['login'] = true; header('LOCATION:moms.php'); die();
+  }
+  if($username !== 'admin')$userError = 'Invalid Username';
+  if($password !== 'password')$passError = 'Invalid Password';
+}
+?>
+<!DOCTYPE HTML>
+<html>
+        <head>
+                <title>Login</title>
+        </head>
+        <body>
+        <form name='input' action='<?php echo $_SERVER['PHP_SELF'];?>' method='POST'>
+                <label for='username'></label><input type ='text' value='<?php echo $username;?>' id='username' name='username'/>
+                <div class='error'><?php echo $username;?></div>
+                <label for='password'></label><input type='password' value='<?php echo $password;?>' id='password' name='password' />
+                <div class='error'><?php echo $passError;?></div>
+                <input type='submit' value='Submit' name='sub' />
+        </form>
+        </body>
+</html>
