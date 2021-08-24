1bddf38221770228ebb5dd0d64b215c471961beb
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-08-24 21:07:53 +0530

    Create merge.sh

---
+++ Onsites/onsites_task1/merge.sh
@@ -0,0 +1,33 @@
+#BEGIN is read only once when first input is given
+awk 'BEGIN{
+        FS=",";
+        } 
+
+FNR==NR{
+# FNR resets to 1 for new file while NR continues
+        array_store_lines[$2]=$0;
+        # Full line is stored to array with $2 as index-employeeId
+        next;
+        # next is used to store all the lines to the array 
+}
+{
+        print array_store_lines[$1] FS $2  >> "merged_temp.txt"#$3} 
+        # Prints the array of $1 index,$2 of file2.txt(salaries) 
+}
+' file1.txt file2.txt 
+
+
+awk 'BEGIN{
+        FS=",";
+        }
+FNR==NR{
+# FNR resets to 1 for new file while NR continues
+        array_store_lines1[$3]=$0;
+        # Full line stored to array with $3 as index-phone
+        next;
+}
+{
+        print array_store_lines1[$2] FS $1 FS $3 > "final_merged.txt"
+        # Prints the array with index $2,email,year of file3.txt
+}
+' merged_temp.txt file3.txt
