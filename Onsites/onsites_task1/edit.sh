cf94bbe993eeadb81d560a9ebe0edc519de1d4e5
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-08-25 01:52:34 +0530

    Update edit.sh

--- Onsites/onsites_task1/edit.sh
+++ Onsites/onsites_task1/edit.sh
@@ -2,9 +2,7 @@
 read -p 'Enter column name to be changed(the column name should be within " "): ' column
 read -p 'Enter the value in that column to be changed: ' value
 read -p 'Enter the changed value to add into file: ' newvalue
-awk -F, -v col=$column -v val=$value -v newval=$newvalue 'NR==1{{for(i=1;i<=NF;i++) if($i==col) {print i;x = i;val }}
+awk -F, -v col=$column 'NR==1{{for(i=1;i<=NF;i++) if($i==col) {print i}}
+}' temp.txt 
 
-if($x==$val) {$x=$newval;print $0 }
-else{print "hi" >> "new.txt"
-}
-}' temp.txt
+sed -i "s/$value/$newvalue/" temp.txt
