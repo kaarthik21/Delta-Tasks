d4e285e5ec966f3ef47a174e4d14e130dc085140
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-08-25 01:53:10 +0530

    Update edit.sh

--- Onsites/onsites_task1/edit.sh
+++ Onsites/onsites_task1/edit.sh
@@ -3,6 +3,6 @@ read -p 'Enter column name to be changed(the column name should be within " "): 
 read -p 'Enter the value in that column to be changed: ' value
 read -p 'Enter the changed value to add into file: ' newvalue
 awk -F, -v col=$column 'NR==1{{for(i=1;i<=NF;i++) if($i==col) {print i}}
-}' temp.txt 
+}' final_merged.txt 
 
-sed -i "s/$value/$newvalue/" temp.txt
+sed -i "s/$value/$newvalue/" final_merged.txt
