22dc38f56e3ea52781f5882a97976f62fb29ebfc
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-08-26 05:36:15 -0400

    Update captcha.php

--- Onsites/onsite_task3/captcha.php
+++ Onsites/onsite_task3/captcha.php
@@ -1,11 +1,6 @@
 <?php
 session_start();  
-#header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");   
-#header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");   
-#header("Cache-Control: no-store, no-cache, must-revalidate");   
-#header("Cache-Control: post-check=0, pre-check=0", false);  
-#header("Pragma: no-cache");   
-  
+
 function _generateRandom($length=6)  
 {  
     $_rand_src = array(  
